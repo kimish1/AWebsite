d2e4baa820814c24279a42e39c6d7ad3b0539c76
Puzenko Igor <bjiast.001@gmail.com>
2025-09-09 18:13:42 +0300

    A website books 7 last 4

--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,14 +1,12 @@
 import Page from '../../layout/page'
 import books from "../../Fixtures/Books.json";
 import authors from "../../Fixtures/Authors.json";
-import categori from "../../Fixtures/Categories.json";
 import Book from "../../components/Book/Book.tsx";
 import "./style.css"
 // @ts-ignore
 import Category from "../../components/Categories/Category.tsx";
 import "../../App.css"
 import {useParams} from "react-router";
-
 
 
 function OneBook() {
@@ -22,6 +20,9 @@ function OneBook() {
         return <div>Книга не знайдена</div>;
     }
 
+    const otherBooks = books.filter((b) => b.id !== bookId);
+    const shuffled = [...otherBooks].sort(() => Math.random() - 0.5);
+    const randomBooks = shuffled.slice(0, 4);
 
     return (
         <div>
@@ -48,13 +49,14 @@ function OneBook() {
                             </div>
                         </button>
                     </a>
-
                 </div>
+
                 <div className="popular-books">
-                    {books.slice(0,4).map((book) => {
+                    {randomBooks.map((book) => {
                         const author = authors.find((author) => author.id === book.authorId);
                         return (
                             <Book
+                                key={book.id}
                                 id={book.id}
                                 title={book.title}
                                 price={book.price}
